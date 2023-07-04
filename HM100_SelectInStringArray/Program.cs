2bbfcf76c32064b816dc4ab1a59785c76f78962d
Catherine <catherine.zelenia@gmail.com>
2023-07-04 14:44:45 +0300

    added Formatted Console Output Method for Array to String _ PrintArray

--- HM100_SelectInStringArray/Program.cs
+++ HM100_SelectInStringArray/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк,
+// длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры,
+// либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,
+// лучше обойтись исключительно массивами.
+
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+// // Метод получения значения с консоли
+// string GetRows (string message)
+// {
+//     System.Console.WriteLine(message);
+//     string result = System.Console.ReadLine();
+//     return result;
+// }
+
+// //образование нового массива
+// void ReadStringToArray(string[] args)
+// {
+
+// }
+
+// //чтение строки
+
+
+// // 1. Объявление переменных
+// int count = 0; // количество строк
+// string[] ArrayMain = new string[count]; // массив строк
+// int numberRowsInArray; // текущее количество строк в массиве
+// string newRows;
+// string[] ArrayNext; // дополнительный массив - сохраняет старый массив строк
+
+// GetRows("Enter strings:");
+
+
+string[] ArrayMain = { "Sunday", "Monday", "Tuersday",
+      "Wednesday", "Thirsday", "Friday", "Saturday" };
+
+// Метод форматированного вывода на консоль для массива в строку
+void PrintArray(string[] array)
+{
+    System.Console.Write("[");
+
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"\"{array[i]}\", ");
+    }
+    //Console.ReadKey();
+    System.Console.Write($"\"{array[^1]}\"");
+    System.Console.WriteLine("]");
+}
+
+PrintArray(ArrayMain);
